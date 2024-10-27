b197beb467300eaf5f744a4d1607d794391d7312
altos721 <altos721@gmail.com>
2024-10-27 15:33:00 +0800

    Add magic-8-ball

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,52 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Ask Me Anything',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blueGrey,
+        ),
+        body: BallPage(),
+      ),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey[500],
+      child: Center(
+        child: Row(
+          children: [
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    ballNumber = Random().nextInt(5) + 1;
+                  });
+                },
+                child: Image.asset('images/ball$ballNumber.png'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+}
